be9fe2c110f3fb61612a5a8d577dddab1da33ce4
retumaru <220109uj@yse-c.net>
2023-12-01 13:09:37 +0900

    課題

---
+++ database/migrations/2023_11_17_030101_create_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+     #db設定
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("name",255);
+            $table->integer("price");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+};
