0c3c8605083d07135d2e53a21b3e7a9a63968d77
retumaru <220109uj@yse-c.net>
2023-12-08 13:07:00 +0900

    課題

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,11 +11,14 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <script src="https://cdn.tailwindcss.com"></script>
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
+            <!-- layouts/navigation.blade.php を読み込み -->
             @include('layouts.navigation')
 
             <!-- Page Heading -->
